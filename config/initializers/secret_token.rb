4214857a11e5b26430c8cb9acc7ed7da25a0df40
Timmy Galvin <tgalvin@mit.edu>
2012-11-13 07:35:19 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mtj::Application.config.secret_token = 'a7b1d7b6c4cb919d6b0e67c292482a6db958df1585d04ab9c03784f0523c5f44ac22e726916584cae5d90becd955058935dbfa488b48df66dd6f80a03db7adaf'

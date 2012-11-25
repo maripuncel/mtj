104760f3be49e49ee52b5e4062e995f326c5a163
Timmy Galvin <tgalvin@mit.edu>
2012-11-24 20:00:50 -0800

    env

--- config/environment.rb
+++ config/environment.rb
@@ -7,11 +7,11 @@ Mtj::Application.initialize!
 Mtj::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'mitjobtalk@gmail.com',
-    :password             => 'zxcvbzxcvb',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'mitjobtalk@gmail.com',
+  :password             => 'zxcvbzxcvb',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
 end

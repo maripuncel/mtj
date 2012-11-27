a13c402238c3cad80e329ed67b1cd5260109c0d4
Timmy Galvin <tgalvin@mit.edu>
2012-11-27 02:59:11 -0800

    new email

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Mtj::Application.configure do
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => 'mitjobtalk@gmail.com',
+  :user_name            => 'mitjobtalk2@gmail.com',
   :password             => 'zxcvbzxcvb',
   :authentication       => 'plain',
   :enable_starttls_auto => true  }

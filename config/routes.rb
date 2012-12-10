f62afc85f81154caf81252101135ca6a892fd6d2
Mari Miyachi <mmiyachi@mit.edu>
2012-12-09 18:07:22 -0800

    add answers works...needs ajax

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ Mtj::Application.routes.draw do
   match 'change_password' => 'users#change_password'
   match 'activate/:serial' => 'sessions#activate'
 
+  match 'questions/:id/create_answer', to: 'questions#create_answer'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
